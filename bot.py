015e2c15a6d829d83330f96a9224f47c146fa4c6
GSri30 <g.sriharsha@iiitb.org>
2021-01-03 15:57:27 +0530

    intermediate

---
+++ bot.py
@@ -0,0 +1,90 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+ADMIN = os.getenv("ADMIN")
+SAC_CHANNEL = os.getenv("SAC_CHANNEL")
+AUTH_CHANNEL = os.getenv("AUTH_CHANNEL")
+
+
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+#! Common commands
+
+@bot.command(name="register",help="Registers a user using their IIIT-B domain mail id.")
+async def register(ctx, mailID:str):
+    if(str(ctx.channel.id)!=AUTH_CHANNEL):
+        await ctx.send(f"This command cannot be used from here.")
+        return
+    await ctx.send(f"{mailID} registered for user {ctx.author} with {ctx.author.id}")
+
+@bot.command(name="couriers",help="Gives you your couriers list.(if any)")
+async def couriers(ctx):
+    await ctx.send(f"Feature coming soon! :smile:")
+
+
+
+#! Admin commands 
+
+@bot.command(name="filter",help="Filters out the unauthorized users.")
+@commands.has_role(ADMIN)
+async def filter(ctx):
+    if(str(ctx.channel.id)!=SAC_CHANNEL):
+        await ctx.send(f"This command cannot be used from here.")
+        return
+    await ctx.send("Filtered and returned the results!")
+
+@bot.command(name="filter-remove",help="Filters out the unauthorized users and removes them from the server.")
+@commands.has_role(ADMIN)
+async def filter_remove(ctx):
+    if(str(ctx.channel.id)!=SAC_CHANNEL):
+        await ctx.send(f"This command cannot be used from here.")
+        return
+    await ctx.send(f"Removed the unauthorized users.\nStored their emails in the database successfully!")
+
+@bot.command(name="ban",help="Bans the specified user from the server.")
+@commands.has_role(ADMIN)
+async def ban(ctx,member:discord.User=None,reason=None):
+    if(str(ctx.channel.id)!=SAC_CHANNEL):
+        await ctx.send(f"This command cannot be used from here.")
+        return
+    if member==None or member==ctx.message.author:
+        await ctx.send("You cannot ban yourself.")
+        return
+    await member.send(f"You have been banned from {ctx.guild.name}. Contact SAC.")
+    return
+
+@bot.command(name="kick",help="Kicks out the specified user from the server.")
+@commands.has_role(ADMIN)
+async def kick(ctx,member:discord.User=None,reason=None):
+    if(str(ctx.channel.id)!=SAC_CHANNEL):
+        await ctx.send(f"This command cannot be used from here.")
+        return
+    if member==None or member==ctx.message.author:
+        await ctx.send("You cannot kick out yourself.")
+        return
+    await member.send(f"You have been kicked out from {ctx.guild.name}. Contact SAC.")
+
+
+#! Error handling
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send(f'You do not have the required permission to use this command. Try contacting SAC!')
+    # elif(isinstance(error, commands.errors.MissingRequiredArgument)):
+    #     await ctx.send(f"{error}")
+    else:
+        await ctx.send(f"{error}")
+
+
+bot.run(TOKEN)
